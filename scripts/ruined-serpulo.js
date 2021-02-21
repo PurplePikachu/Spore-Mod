ab3b600bc766b34f7aa608aee5f6aa8e1154ad25
PurplePikachu <78454948+PurplePikachu@users.noreply.github.com>
2021-02-21 13:19:10 +0800

    Create ruined-serpulo.js

---
+++ scripts/ruined-serpulo.js
@@ -0,0 +1,34 @@
+const serpulo2 = extend(Planet, "serpulo2", Planets.sun, 1, 0.5, {
+    generator: new SerpuloPlanetGenerator(),
+    bloom: true,
+    radius: 2,
+    accessible: true,
+    hasAtmosphere: true,
+    atmosphereColor: Color.valueOf("#808080"),
+    atmosphereRadIn: 0.02,
+    atmosphereRadOut: 0.3,
+    localizedName: "Ruined Serpulo"
+    
+    const imp7800 = extend(SectorPreset, imp7800", serpulo2, 78, {
+    captureWave: 40,
+    localizedName: "Impact 7800",
+    difficulty: 3,
+    alwaysUnlocked: true
+});
+
+    const scrap2= extend(SectorPreset, scrap2", serpulo2, 271, {
+    captureWave: 25,
+    localizedName: "Scrap World",
+    difficulty: 8,
+});
+    const scrap2= extend(SectorPreset, scrap2", serpulo2, 93, {
+    captureWave: 35,
+    localizedName: "Studier Complex",
+    difficulty: 8,
+});
+module.exports = {
+    serpulo2: serpulo2,
+    scrap2: Scrap World,
+    imp7800: Impact 7800,
+    study: Studier Complex
+}
