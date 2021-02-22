50262da3fda4823e426ea36034fdbd5cbaaeb78b
PurplePikachu <78454948+PurplePikachu@users.noreply.github.com>
2021-02-22 13:17:27 +0800

    Update Ruinedserpulo.js

--- scripts/Ruinedserpulo.js
+++ scripts/Ruinedserpulo.js
@@ -11,13 +11,18 @@ const ruinedserpulo = extend(Planet, "ruinedserpulo", Planets.sun, 1, 0.5, {
     localizedName: "Ruined Serpulo"
 });
 ruinedserpulo.meshLoader = () => extend(HexMesh, ruinedserpulo, 6, {});
+
+    const ground1 = extend(SectorPreset, "ground1", ruinedserpulo, 15, {
+    captureWave: 20,
+    localizedName: "Ground One",
+    difficulty: 1,
+    description: "First zone, first RUINED zone.
     
     const imp7800 = extend(SectorPreset, "imp7800", ruinedserpulo, 78, {
     captureWave: 40,
     localizedName: "Impact 7800",
     difficulty: 3,
     description: "Impact 0078, partially conquered. You get the idea, Sufficient amount of resources, Criss-crossing walls making it difficult to fully expand base, Enemy has one spawn point with air attacks, yadda yadda yadda."
-    alwaysUnlocked: true
 });
 
     const scrapworld= extend(SectorPreset, "scrapworld", ruinedserpulo, 271, {
@@ -36,5 +41,6 @@ module.exports = {
     ruinedserpulo: Ruinedserpulo,
     scrapworld: Scrap World,
     imp7800: Impact7800,
-    study: Studier Complex
+    study: Studier Complex,
+    ground1: GroundOne
 }
