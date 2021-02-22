a08cbc29ca12ae76fb85a47322bf2f85f43b0f2b
PurplePikachu <78454948+PurplePikachu@users.noreply.github.com>
2021-02-22 12:52:39 +0800

    Update ruined-serpulo.js

--- scripts/ruined-serpulo.js
+++ scripts/ruined-serpulo.js
@@ -9,22 +9,25 @@ const ruinedserpulo = extend(Planet, "ruinedserpulo", Planets.sun, 1, 0.5, {
     atmosphereRadOut: 0.3,
     localizedName: "Ruined Serpulo"
     
-    const imp7800 = extend(SectorPreset, imp7800", ruinedserpulo, 78, {
+    const imp7800 = extend(SectorPreset, "imp7800", ruinedserpulo, 78, {
     captureWave: 40,
     localizedName: "Impact 7800",
     difficulty: 3,
+    description: "Impact 0078, partially conquered. You get the idea, Sufficient amount of resources, Criss-crossing walls making it difficult to fully expand base, Enemy has one spawn point with air attacks, yadda yadda yadda."
     alwaysUnlocked: true
 });
 
-    const scrapworld= extend(SectorPreset, scrapworld", ruinedserpulo, 271, {
+    const scrapworld= extend(SectorPreset, "scrapworld", ruinedserpulo, 271, {
     captureWave: 25,
     localizedName: "Scrap World",
+    description: "Wait, that's not 271! That's completely different than before!"
     difficulty: 8,
 });
-    const study= extend(SectorPreset, study", ruinedserpulo, 93, {
+    const study= extend(SectorPreset, "study", ruinedserpulo, 93, {
     captureWave: 35,
     localizedName: "Studier Complex",
-    difficulty: 8,
+    description: "Planetary Launch Terminal. Hope you are familiar with this!"
+    difficulty: 10,
 });
 module.exports = {
     ruinedserpulo: ruinedserpulo,
