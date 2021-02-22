20e0f3fb5487fc41786b9531c52d34f6523096ab
PurplePikachu <78454948+PurplePikachu@users.noreply.github.com>
2021-02-22 12:58:40 +0800

    Update ruined-serpulo.js

--- scripts/ruined-serpulo.js
+++ scripts/ruined-serpulo.js
@@ -10,7 +10,7 @@ const ruinedserpulo = extend(Planet, "ruinedserpulo", Planets.sun, 1, 0.5, {
     atmosphereRadOut: 0.3,
     localizedName: "Ruined Serpulo"
 });
-templura.meshLoader = () => extend(HexMesh, templura, 6, {});
+ruinedserpulo.meshLoader = () => extend(HexMesh, ruinedserpulo, 6, {});
     
     const imp7800 = extend(SectorPreset, "imp7800", ruinedserpulo, 78, {
     captureWave: 40,
